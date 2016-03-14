cf5fa9687ce3cd127a9cec35f16122df3e4f8688
cadaeic <roadrunner.x@outlook.com>
2016-03-14 08:44:48 -0400

    Fix encoding and line-endings

--- mods/seasonal/abilities.js
+++ mods/seasonal/abilities.js
@@ -1,4 +1,4 @@
-"use strict";
+﻿"use strict";
 
 exports.BattleAbilities = {
 	// Frysinger
--- mods/seasonal/scripts.js
+++ mods/seasonal/scripts.js
@@ -1,4 +1,4 @@
-"use strict";
+﻿"use strict";
 
 exports.BattleScripts = {
 	randomSeasonalMeleeTeam: function (side) {
